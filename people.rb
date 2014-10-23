0b0932f6349a694bff0f41f11ae12becba150d73
Yung Dai <yungchidai@gmail.com>
2014-10-23 13:48:30 -0400

    4. Both the instructor and the student have names. We know that instructors and students are both people. Create a parent Person class that contains the attribute name and an initializer to set the name.

--- people.rb
+++ people.rb
@@ -14,3 +14,13 @@ class Instructor
     puts "Everything in Ruby is an Object"
   end
 end
+
+# 4. Both the instructor and the student have names. We know that instructors and students
+# are both people. Create a parent Person class that contains the attribute name and
+# an initializer to set the name.
+
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
