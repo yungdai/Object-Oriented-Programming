d7dc7ab301430cfc61327612d0b73080e73d0d3b
Yung Dai <yungchidai@gmail.com>
2014-10-23 13:38:39 -0400

    2. The student class has a method called learn that outputs "I get it!".

--- people.rb
+++ people.rb
@@ -1,5 +1,9 @@
 # 1. Let's start by creating two classes: one called Student and another called Instructor.
 class Student
+  # 2. The student class has a method called learn that outputs "I get it!".
+  def learn
+    puts "I get it!"
+  end
 
 end
 
