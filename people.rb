de6aea7a2d9af2d912df5a7beeb2f00d1edef919
Yung Dai <yungchidai@gmail.com>
2014-10-23 14:07:25 -0400

    # 6. Create an instance of Instructor whose name is "Chris" and call his greeting.

--- people.rb
+++ people.rb
@@ -1,20 +1,3 @@
-# 1. Let's start by creating two classes: one called Student and another called Instructor.
-class Student
-  # 2. The student class has a method called learn that outputs "I get it!".
-  def learn
-    puts "I get it!"
-  end
-
-end
-
-class Instructor
-  # 3. The instructor class has a method called teach that outputs "Everything in Ruby
-  # is an Object".
-  def teach
-    puts "Everything in Ruby is an Object"
-  end
-end
-
 # 4. Both the instructor and the student have names. We know that instructors and students
 # are both people. Create a parent Person class that contains the attribute name and
 # an initializer to set the name.
@@ -28,9 +11,32 @@ class Person
   # my name is #{name}". Where's the best place to put this common method?
 
   def greeting
-    puts "Hi, my name is #{name}."
+    puts "Hi, my name is #{@name}."
   end
 end
 
+# 1. Let's start by creating two classes: one called Student and another called Instructor.
+class Student < Person
+  # 2. The student class has a method called learn that outputs "I get it!".
+  def learn
+    puts "I get it!"
+  end
+
+end
+
+class Instructor < Person
+  # 3. The instructor class has a method called teach that outputs "Everything in Ruby
+  # is an Object".
+
+  def teach
+    puts "Everything in Ruby is an Object"
+  end
+end
+
+
+
+# 6. Create an instance of Instructor whose name is "Chris" and call his greeting.
+chris = Instructor.new("Chris")
+chris.teach
 
 
